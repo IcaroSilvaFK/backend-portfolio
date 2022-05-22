51b4df4094ce32c9bb96282e20921a828704ff9c
Icaro Vieira <icarovsilva1@gmail.com>
2022-05-22 15:21:17 -0300

    remove middleware

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,12 +3,11 @@ import { Router } from "express";
 import { DeleteUserRoute } from "../interceptors/deleteUser";
 import { GetUsersRoute } from "../interceptors/getUsers";
 import { NewUser } from "../interceptors/newUser";
-import { HeaderAllow } from "../middleware/headerAllow";
 
 const routes = Router();
 
 routes.post("/", NewUser.NewUserRoute);
-routes.get("/", HeaderAllow, GetUsersRoute.getAllUsers);
-routes.delete("/:id", HeaderAllow, DeleteUserRoute.deleteUser);
+routes.get("/", GetUsersRoute.getAllUsers);
+routes.delete("/:id", DeleteUserRoute.deleteUser);
 
 export { routes };
